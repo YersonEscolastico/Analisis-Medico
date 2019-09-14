1886f6d879694d1a80151851e29b08060e210974
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-09-15 06:18:51 +0800

    Agregando entidad TipoAnalisis

---
+++ Entidades/TipoAnalisis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class TipoAnalisis
+    {
+
+        [Key]
+        public int TiposId { get; set; }
+        public string Analisis { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public TipoAnalisis()
+        {
+            TiposId = 0;
+            Analisis = string.Empty;
+            FechaRegistro = DateTime.Now;
+        }
+    }
+}
