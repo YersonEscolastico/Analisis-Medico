b83a1369630e721cce0159a2c80b137c5899df6e
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-09-15 06:24:02 +0800

    Agregando entidad Analisis

---
+++ Entidades/AnalisisDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    [Serializable]
+    public class AnalisisDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public string Resultado { get; set; }
+        public string Analisis { get; set; }
+        public AnalisisDetalle()
+        {
+            DetalleId = 0;
+            Analisis = string.Empty;
+            Resultado = string.Empty;
+        }
+        public AnalisisDetalle(string analisis, string resultado)
+        {
+            Analisis = analisis;
+            Resultado = resultado;
+        }
+    }
+}
