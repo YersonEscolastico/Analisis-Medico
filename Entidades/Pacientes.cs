c695767b8b854250bc67c7e0e66ae55a633b6058
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-09-15 06:13:05 +0800

    Agregando Entidad Pacientes

---
+++ Entidades/Pacientes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Pacientes
+    {
+        [Key]
+        public int PacienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public Pacientes()
+        {
+            PacienteId = 0;
+            Nombres = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+        }
+    }
+}
---
+++ pAnalisisMD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pAnalisisMD.Global" Language="C#" %>
