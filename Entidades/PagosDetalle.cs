94d9f09a79c9c228413c90817a7e113a1f461a04
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-09-16 08:43:39 +0800

    Agregando entidad de pago

---
+++ Entidades/PagosDetalle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class PagosDetalle
+    {
+        public int DetallePagoId { get; set; }
+        public int PagoId { get; set; }
+        public int AnalisisId { get; set; }
+        public decimal MontoPago { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+        public PagosDetalle()
+        {
+            DetallePagoId = 0;
+            PagoId = 0;
+            AnalisisId = 0;
+            MontoPago = 0;
+            FechaRegistro = DateTime.Now;
+        }
+        public PagosDetalle( int analisisId, decimal montoPago)
+        {
+            AnalisisId = analisisId;
+            MontoPago = montoPago;
+            FechaRegistro = DateTime.Now;
+        }
+    }
+}
